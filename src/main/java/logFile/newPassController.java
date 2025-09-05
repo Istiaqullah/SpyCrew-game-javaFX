038f1ecd7d4e02cfc96dcf149239d131884eb735
Istiaqullah <mohammadistiaqullah@gmail.com>
2025-09-05 20:27:01 +0600

    11th commit

---
+++ src/main/java/logFile/newPassController.java
@@ -0,0 +1,13 @@
+package logFile;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class newPassController {
+    @FXML
+    void logIn(ActionEvent event)
+    {
+        utils.changeScene(event,"login.fxml",null,null);
+
+    }
+}
