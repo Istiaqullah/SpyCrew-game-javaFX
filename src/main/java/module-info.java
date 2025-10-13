808b40fca1359fe31b07654972f1262be935f21d
Abul Hasnat Mohammad Istiaqullah <mohammadistiaqullah@gmail.com>
2025-10-13 11:29:35 +0600

    module update(13/10/2025)

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,16 +1,15 @@
-module Log  {
+module Log{
     requires javafx.controls;
     requires javafx.fxml;
     requires javafx.graphics;
     requires javafx.base;
     requires java.sql;
     requires javafx.media;
+    requires java.desktop;
 
 
     opens Log to javafx.fxml;
     exports Log;
-    opens Profile to javafx.fxml;
-    exports Profile;
     exports main;
     opens main to javafx.fxml;
 }
