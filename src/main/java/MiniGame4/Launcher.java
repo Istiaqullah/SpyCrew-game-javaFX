14108722d6c53f8e66f83b27c5fbd8eceea4eb94
Abul Hasnat Mohammad Istiaqullah <mohammadistiaqullah@gmail.com>
2025-11-09 18:38:27 +0600

    Car Dodge Game test launcher

---
+++ src/main/java/MiniGame4/Launcher.java
@@ -0,0 +1,8 @@
+package MiniGame4;
+import javafx.application.Application;
+
+public class Launcher {
+    public static void main(String[] args) {
+        Application.launch(CarDodgeApp.class, args);
+    }
+}
