10595b3733f6d4c53abbeee95c3f34b41f43c085
Abul Hasnat Mohammad Istiaqullah <mohammadistiaqullah@gmail.com>
2025-11-10 14:11:09 +0600

    Update (10/11/25)

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -10,8 +10,20 @@ module Log{
 
     opens Log to javafx.fxml;
     exports Log;
+
     exports main;
     opens main to javafx.fxml;
+
     opens MiniGame1 to javafx.fxml;
     exports  MiniGame1;
+
+opens MiniGame2 to javafx.fxml;
+    exports MiniGame2;
+
+    opens MiniGame3 to javafx.fxml;
+    exports MiniGame3;
+
+    opens MiniGame4 to javafx.fxml;
+    exports MiniGame4;
+
 }
